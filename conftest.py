f175c91bf7b1593a9dec128eeb35738161bfc106
Alex <igea@bk.ru>
2020-08-22 20:36:57 +0300

    Задание №26 (FTP video 9.2)

--- conftest.py
+++ conftest.py
@@ -25,18 +25,11 @@ def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
     # web_config = load_config(request.config.getoption("--target"))["web"]#вместо строки используем фикстуру "конфиг"
-    # web_config = config["web"]# config["web"] ставим вместо имени переменной
+    # web_config = config["web"]# config["web"] ставим вместо имени переменной в строке №30
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=config["web"]["baseUrl"])
-    # fixture.session.ensure_login(username=config["username"], password=config["password"])
+    # fixture.session.ensure_login(username=config["webadmin"]["username"], password=config["webadmin"]["password"])
     return fixture
-
-# @pytest.fixture(scope="session", autouse=True)
-# def configure_server(request, config):
-#     install_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
-#     def fin():
-#         restore_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
-#     request.addfinalizer(fin)
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
@@ -51,17 +44,24 @@ def pytest_addoption(parser):
     # parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
 
-# def install_server_configuration(host, username, password):
-#     with ftputil.FTPHost(host, username, password) as remote:
-#         if remote.path.isfile("config_inc.php.bak"):
-#             remote.remove("config_inc.php.bak")
-#         if remote.path.isfile("config_inc.php"):
-#             remote.rename("config_inc.php", "config_inc.php.bak")
-#         remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
-#
-# def restore_server_configuration(host, username, password):
-#     with ftputil.FTPHost(host, username, password) as remote:
-#         if remote.path.isfile("config_inc.php.bak"):
-#             if remote.path.isfile("config_inc.php"):
-#                 remote.remove("config_inc.php")
-#             remote.rename("config_inc.php.bak", "config_inc.php")
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+    def fin():
+        restore_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+    request.addfinalizer(fin)
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
